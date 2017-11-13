81af992d1625e28c84cdb9703d6d0edf2072f717
Emily Gordon <emgordon154@gmail.com>
2017-11-13 14:36:10 -0500

    got back from lunch

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,15 @@ const {wikiRouter} = require('./wiki.js')
 router.use('/wiki', wikiRouter)
 router.use('/user', userRouter)
 
+router.get('/', function(res,req,next){
+  Page.findAll()
+    .then(function(foundPages) {
+      console.log(foundPages)
+      res.render('index', {pages: foundPages})
+    })
+
+})
+
 module.exports = {
   router: router,
 }
