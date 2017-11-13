020a9989d5e7371af1fb0bf46501ecbd3d8066d7
Emily Gordon <emgordon154@gmail.com>
2017-11-13 16:02:23 -0500

    end of workshop v_v

--- routes/wiki.js
+++ routes/wiki.js
@@ -8,26 +8,32 @@ wikiRouter.post('/', function (req, res, next) {
 
   var title = req.body.title
   var content = req.body.content
-  var author = User.findOrCreate({
-    where:
-    {
+  
+  User.findOrCreate({
+    where: {
       name: req.body.author,
       email: req.body.email
     }
   })
-    .then(function (author) {
+    .then(function (values) {
+
+      var user = values[0];
+
       var page = Page.build({
+        title: req.body.title,
+        content: req.body.content
+      });
 
-        title: title,
-        content: content,
-        author: author.name
+      return page.save().then(function (page) {
+        return page.setAuthor(user);
+      });
 
-      })
-      page.save().then((savedPage) => res.redirect(savedPage.route))
-        .catch(next);
-      // -> after save -> res.redirect('/');
-    });
-});
+    })
+    .then(function (page) {
+      res.redirect(page.route);
+    })
+    .catch(next);
+})
 
 
 // STUDENT ASSIGNMENT:
@@ -50,13 +56,15 @@ wikiRouter.get('/:urlTitle', function (req, res, next) {
     }
   }).then(function (foundPage) {
     //console.log(foundPage.title)
-    res.render('wikipage', {
-      title: foundPage.title,
-      content: foundPage.content,
-      author: foundPage.author
-    });
-  })
-    .catch(next);
+    foundPage.getAuthor().then(function(author){
+      console.log(author.name)
+      res.render('wikipage', {
+        title: foundPage.title,
+        content: foundPage.content,
+        author: author.name
+      });
+    })
+  }).catch(next);
 })
 
 
